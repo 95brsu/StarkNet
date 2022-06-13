c905282b97b554761c716a0441102648aadec6bd
95brsu <103598894+95brsu@users.noreply.github.com>
2022-06-13 15:22:58 +0300

    Update update_starknet.sh

--- update_starknet.sh
+++ update_starknet.sh
@@ -16,7 +16,7 @@ sleep 2
 
 
 function line {
-  echo "----06.06.2022--------------------------------------------------------------------"
+  echo "----13.06.2022--------------------------------------------------------------------"
 }
 
 function colors {
@@ -26,10 +26,10 @@ function colors {
 }
 
 
-function install_stark {
+function update_stark {
 cd ~/pathfinder
 git fetch
-git checkout v0.2.0-alpha
+git checkout v0.2.1-alpha
 cargo build --release --bin pathfinder
 mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
 cd py
@@ -37,40 +37,18 @@ source .venv/bin/activate
 PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
   }
 
-
-
-function build_pd {
-cd $HOME/penumbra/
-sudo apt-get install clang
-cargo build --release --bin pd
-cargo run --quiet --release --bin pcli balance
-}
-
-
 function restart {
   systemctl restart starknetd
   pathfinder -V
 }
 
-function rust_update {
-  rustup update
-  rustup default nightly
-}
-
-function tendermint {
-  tendermint init full 
-  curl -s http://testnet.penumbra.zone:26657/genesis | jq ".result.genesis" > $HOME/.tendermint/config/genesis.json
-}
-
-
-
 
 colors
 
 line
-echo -e "${RED}Начинаем обновление StarkNet ${NORMAL}"
+echo -e "${RED}1/2Начинаем обновление StarkNet ${NORMAL}"
 line
-install_stark
+update_stark
 line
 echo -e "${GREEN}2/2 Перезагрузка ${NORMAL}"
 line
